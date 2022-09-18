04d249f79ab04dde0a04a6e7f4abdcf7211f2adf
Anton Ilyin <ilyin.a.e@gmail.com>
2022-09-18 12:02:13 +0300

    Added Ex013

---
+++ Ex013/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите число");
+string str = Console.ReadLine();
+if (str.Length < 3)
+{
+    Console.WriteLine("третьей цифры нет");
+    return;    
+}
+    Console.WriteLine(str[2]);
